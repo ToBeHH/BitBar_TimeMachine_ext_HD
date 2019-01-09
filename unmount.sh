f8c2c8a35c4a07562b77ec4a2a608417412cd685
Tobias Schulz-Hess <tobias.schulz-hess@xing.com>
2019-01-09 14:55:21 +0100

    Fixed bash execution - always remember: RTFM!

--- unmount.sh
+++ unmount.sh
@@ -3,6 +3,7 @@
 # <bitbar.title>Auto remove HD after backup</bitbar.title>
 # <bitbar.version>v0.1</bitbar.version>
 # <bitbar.author>Tobias Schulz-Hess</bitbar.author>
+# <bitbar.author.github>ToBe_HH</bitbar.author.github>
 # <bitbar.desc>Checks, if time machine backup is running and if so, ejects HD</bitbar.desc>
 # <bitbar.dependencies>bash/bitbar.dependencies>
 # <bitbar.abouturl>https://github.com/ToBeHH/BitBar_TimeMachine_ext_HD</bitbar.abouturl>
@@ -23,12 +24,12 @@ if [ $? = 0 ]
 #      else
 #             echo "↻|color=#88FF88"
 #      fi
-   echo "↻|color=#88FF00"
+   echo "↻|color=#88FF88"
    echo "---"
-   echo "Unmount | bash='/usr/sbin/diskutil unmount $TMUUID' terminal=false"
-   echo "TimeMachine | bash='/usr/bin/open -b com.apple.systempreferences /System/Library/PreferencePanes/TimeMachine.prefPane' terminal=false"
+   echo "Unmount | bash='/usr/sbin/diskutil' param1=unmount param2='$TMUUID' terminal=false refresh=true"
+   echo "TimeMachine | bash='/usr/bin/open' param1=-b param2=com.apple.systempreferences param3='/System/Library/PreferencePanes/TimeMachine.prefPane' terminal=false"
 else
    echo "Backup HD not inserted|color=#888888"
    echo "---"
-   echo "Mount | bash='/usr/sbin/diskutil mount $TMUUID' terminal=false"
+   echo "Mount | bash='/usr/sbin/diskutil' param1=mount param2='$TMUUID' terminal=false refresh=true"
 fi
